f5fc8ef6cef279e471c9e81108f6a53d6218794b
kritagyakhandelwal314e <kritagya.khandelwal@314ecorp.com>
2021-06-29 05:19:58 +0530

    Part 1: Partial

---
+++ part 1/main.py
@@ -0,0 +1,72 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+providers = []
+
+class IdGenerator():
+  def __init__(self):
+    self.id = 0
+  def __iter__(self):
+    return self
+  def __next__(self):
+    self.id += 1
+    return self.id
+
+id_generator = IdGenerator()
+
+class Provider(BaseModel):
+  id: int
+  active: bool
+  name: str
+  qualification: str
+  speciality: str
+  phone: str
+  department: str
+  organisation: str
+  location: str
+  address: str
+
+
+@app.get('/providers')
+async def get_providers():
+  return providers
+
+@app.get('/providers/{provider_id}')
+async def get_provider(provider_id: int, status_code=200):
+  for provider in providers:
+    if provider['id'] == provider_id:
+      return provider
+  return {
+    "error": {
+      "message": "No such Provider",
+      "status": 404
+    }
+  }
+
+@app.post('/providers/')
+async def add_provider(provider: Provider):
+  provider.id = next(id_generator)
+  providers.append(provider.dict())
+  return providers[-1]
+
+@app.delete('/providers/{provider_id}')
+async def delete_provider(provider_id: int):
+  idx = -1
+  for i, provider in enumerate(providers):
+    if provider['id'] == provider_id:
+      idx = i
+      break
+  if idx == -1:
+    return {
+      "error": {
+        "message": "No such Provider",
+        "status": 404
+      }
+    }
+  provider = providers.pop(idx)
+  return provider
+
+
